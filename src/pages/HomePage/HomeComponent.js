9c741d547cdcbd7227fc89cad93e010d77222b85
Shankar <neredimellishankar4@gmail.com>
2026-02-12 15:59:29 +0530

    Adjust game card image to show all fruits with white background

--- src/pages/HomePage/HomeComponent.js
+++ src/pages/HomePage/HomeComponent.js
@@ -779,7 +779,7 @@ export default function HomeComponent() {
                 <img
                   src={fruits}
                   alt="Fruit Ninja Game"
-                  className="w-full h-60 object-cover rounded"
+                  className="w-full h-60 object-contain bg-white rounded p-1"
                 />
                 <Badge className="absolute top-3 left-3 bg-rose-600 text-gray-100 rounded px-1 py-1">Game</Badge>
                 <Badge className="absolute top-3 right-3 bg-amber-500/90 text-gray-100 rounded px-1 py-1">+50 XP</Badge>
